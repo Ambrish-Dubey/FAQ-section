922c68b50da12cfc46fdce1786a6fb6a7f8d06d5
ambrish <ambrishpradeep1000@gmail.com>
2024-11-24 13:50:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PanelModule } from 'primeng/panel';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PanelModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'div';
+        
+  header = 'what type of retail spaces are avialable in malls?'
 }
+
+
